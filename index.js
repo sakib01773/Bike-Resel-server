19816521a91e0f30304c600f71c7e81240e66f2b
sakib01773 <sakibshohan1@gmail.com>
2022-11-28 01:27:51 +0600

    dtabase connected to server

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT ||5000
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config()
+
+
+//middleare
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello from resell bike store!')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.llsjgoo.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.listen(port, () => {
   console.log(`resell bike store listening on port ${port}`)
