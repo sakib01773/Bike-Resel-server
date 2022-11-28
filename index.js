d2b83bed141f0a8bbbe4c48de6334d183f1f9d26
sakib01773 <sakibshohan1@gmail.com>
2022-11-29 01:38:56 +0600

    post product api done

--- index.js
+++ index.js
@@ -21,6 +21,35 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+
+        const usersCollection = client.db('resellPortal').collection('users')
+
+        const productsCollection = client.db('resalePortal').collection('products')
+
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+      
+        app.post('/products', async(req, res)=>{
+          const product = req.body;
+          const result = await productsCollection.insertOne(product)
+          res.send(result)
+      })
+
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.log)
+
 
 
 app.listen(port, () => {
